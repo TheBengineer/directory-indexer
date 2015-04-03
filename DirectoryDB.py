dd33d1ddbe9dcaa974d3537d9fd43d2d71203d87
Ben Holleran <Wild_Doogy@yahoo.com>
2015-04-03 11:58:36 -0400

    This might actually work.

--- DirectoryDB.py
+++ DirectoryDB.py
@@ -5,6 +5,7 @@ from threading import Thread
 from threading import Lock
 import cPickle
 import time
+import os
 
 
 """
@@ -14,26 +15,41 @@ This database will hold all the files and paths that are normally written to a C
 
 
 class DirectoryDB(Thread):
-    def __init__(self):
+    def __init__(self, file_path):
         # TODO make this save data more efficiently
         Thread.__init__(self)
         self.lock = Lock()
+        self.local_lock = Lock()
         self.lock.acquire()
-        self.DB = lite.connect(self.filename+".db", check_same_thread=False)
+        self.file_path = file_path
+        self.DB = lite.connect(self.file_path, check_same_thread=False)
         self.DB_cursor = self.DB.cursor()
         self.lock.release()
+        self.create_table()
+        self.files_to_add = []
+        self.files_to_delete = []
+        self.folders_to_delete = []
+        """
+        :type self.lock: threading.Lock
+        :type self.local_lock: threading.Lock
+        """
 
     def create_table(self):
-        create_updates = "CREATE TABLE Updates " \
-                         "(sector_id INT, body_id TEXT, attribute INT, data INT, dataF REAL, dataS TEXT, dataB BLOB, " \
-                         "PRIMARY KEY (sector_id, body_id, attribute));"
-        # Multiple fields to hold the data. Three will be null.
+        create_updates = "CREATE TABLE files " \
+                         "(path TEXT, filename TEXT);"
         self.lock.acquire()
         tables = self.DB_cursor.execute("SELECT name FROM sqlite_master"
                                         " WHERE type='table' AND name='Updates';").fetchall()
         if tables == []:
             self.DB_cursor.execute(create_updates)
         self.lock.release()
+
+    def add_file(self, file_path):
+        path, filename = os.path.split(file_path)
+        if filename and path:
+            self.local_lock.acquire()
+            self.files_to_add.append([path, filename])
+            self.local_lock.release()
 
     def run(self):
         """
@@ -44,69 +60,45 @@ class DirectoryDB(Thread):
 
         self.go = 1
         while self.go:
+            self.local_lock.acquire()
+            if len(self.files_to_add):
+                for path, filename in self.files_to_add:
+                    query = "INSERT OR REPLACE INTO Updates VALUES(path, '{path}', filename, {filename}" \
+                    " );".format(path=path, filename=filename)
+                    self.lock.acquire()
+                    self.DB_cursor.execute(query)
+                    self.lock.release()
+            self.files_to_add = []
+            if len(self.files_to_delete):
+                for path, filename in self.files_to_delete:
+                    query = "DELETE FROM Updates WHERE path ='{path}' and filename ='{filename}'" \
+                    " ;".format(path=path, filename=filename)
+                    self.lock.acquire()
+                    self.DB_cursor.execute(query)
+                    self.lock.release()
+            self.files_to_delete = []
+            if len(self.folders_to_delete):
+                for path in self.folders_to_delete:
+                    query = "DELETE FROM Updates WHERE path LIKE '{path}%'".format(path=path)
+                    self.lock.acquire()
+                    self.DB_cursor.execute(query)
+                    self.lock.release()
+            self.folders_to_delete = []
+            self.local_lock.release()
             self.writeout()
             # TODO is 30 seconds a good time for the database to be written to?
-            time.sleep(30)
+            time.sleep(.1)
 
 
-    def save_attribute(self, sector_id, body_id, attribute_id, data):
-        """
-        This function will save an updated attribute
-        :param sectorID:
-        :param bodyID:
-        :param data:
-        :return:
-        """
-
-        payload = None
-        payload_complex = 0
-        if type(data) == int:
-            payload = str(data)+", Null, Null, Null"
-        elif type(data) == float:
-            payload = "Null, "+str(data)+", Null, Null"
-        elif type(data) == str:
-            payload = "Null, Null, '"+data+"', Null"
-        else:
-            payload = "Null, Null, Null, ?"
-            payload_complex = 1
-
-        query = "INSERT OR REPLACE INTO Updates VALUES({sector_id}, '{body_id}', {attribute_id}, {payload}" \
-                " );".format(sector_id=sector_id, body_id=body_id, attribute_id=attribute_id, payload=payload)
-        self.lock.acquire()
-        if payload_complex:
-            self.DB_cursor.execute(query, (lite.Binary(cPickle.dumps(data, cPickle.HIGHEST_PROTOCOL)),))
-        else:
-            self.DB_cursor.execute(query)
-        self.lock.release()
-
-    def get_changes(self, sector_id):
-        """
-        This function returns the ID and seed of a sector
-        :return str:
-        """
-        query = "SELECT body_id, attribute, data, dataF, dataS, dataB FROM Updates WHERE sector_id={sector_id};" \
-                "".format(sector_id=sector_id)
+    def get_folders(self, filename):
+        query = "SELECT path, filename WHERE filename = '{filename}';".format(filename=filename)
         self.lock.acquire()
         self.DB_cursor.execute(query)
         data = self.DB_cursor.fetchall()
         self.lock.release()
         return data
 
-    def make_id(self, x, y, z):
-        return (int(x)*1000000) + (int(y)*1000) + int(z)
-
-    def get_coordinates(self, ID):
-        ID = int(ID)
-        x = (ID - (ID % 1000000))/1000000
-        y = ((ID - (ID % 1000))/1000) % 1000
-        z = ID % 1000000
-        print (x, y, z)
-        return x, y, z
-
     def writeout(self):
         self.lock.acquire()
         self.DB.commit()
         self.lock.release()
-
-    def make_body_id(self):
-        pass
