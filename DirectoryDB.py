54075621bfed99b97a523b72b6520ca29c92d940
Ben Holleran <Wild_Doogy@yahoo.com>
2015-04-03 16:37:58 -0400

    Added back in the locks. Was not the issue.

--- DirectoryDB.py
+++ DirectoryDB.py
@@ -48,15 +48,21 @@ class DirectoryDB(Thread):
 
     def add_file(self, file_path):
         path, filename = os.path.split(file_path)
+        self.local_lock.acquire()
         self.add_fileB(path, filename)
+        self.local_lock.release()
 
     def add_fileB(self, path, filename):
         if filename and path:
+            self.local_lock.acquire()
             self.files_to_add.append([path, filename])
+            self.local_lock.release()
 
     def del_folder(self, folder_path):
         if folder_path:
+            self.local_lock.acquire()
             self.folders_to_delete.append(folder_path)
+            self.local_lock.release()
 
     def del_file(self, file_path):
         path, filename = os.path.split(file_path)
