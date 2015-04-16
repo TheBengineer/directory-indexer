2da98833cf327b83127c0b35b91143ebc94878bc
Ben Holleran <Wild_Doogy@yahoo.com>
2015-04-16 11:14:41 -0400

    Adding button functionality. Seems to be working well so far.

--- GUI.py
+++ GUI.py
@@ -3,6 +3,10 @@ __author__ = 'boh01'
 from threading import Thread
 import os
 import Tkinter as tk
+import tkFileDialog
+import subprocess
+
+import DirectoryDB
 
 
 class Window(Thread):
@@ -10,9 +14,11 @@ class Window(Thread):
         Thread.__init__(self)
 
         self.window = tk.Tk()  # Init
-        self.window.geometry("400x500+300+300")
+        self.window.geometry("800x500+300+300")
         self.window.title("Fujifilm Dimatix File Index Database - Ben Holleran April 2014")
         self.window.protocol("WM_DELETE_WINDOW", self.onQuit)
+
+        self.scanner = DirectoryDB.DirectoryDB("C:/tmp/Monster.db")
 
         # ################ Menu
 
@@ -33,11 +39,11 @@ class Window(Thread):
 
         self.scan_frame = tk.Frame(self.window)
         self.scan_frame.config()
-        self.scan_button = tk.Button(self.scan_frame, text="Scan")
+        self.scan_button = tk.Button(self.scan_frame, text="Scan", command=self.start_scan)
         self.scan_button.pack(side=tk.LEFT)
         self.scan_text = tk.Entry(self.scan_frame, font="courier 14")
         self.scan_text.config(width=300)
-        self.scan_browse = tk.Button(self.scan_frame, text="Browse")
+        self.scan_browse = tk.Button(self.scan_frame, text="Browse", command=self.browse_scan_path)
         self.scan_browse.pack(side=tk.RIGHT)
         self.scan_text.pack()
         self.scan_frame.pack(fill=tk.X)
@@ -52,24 +58,25 @@ class Window(Thread):
 
         self.search_frame = tk.Frame(self.window)
         # self.search_frame.config(borderwidth=4, relief=tk.GROOVE) # layout
-        self.search_button = tk.Button(self.search_frame, text="Search")
+        self.search_button = tk.Button(self.search_frame, text="Search", command=self.search)
         self.search_text = tk.Entry(self.search_frame, width=300, font="courier 14")
+        self.search_text.bind('<Return>', self.search)
         self.search_button.pack(side=tk.LEFT)
         self.search_text.pack()
         self.search_frame.pack(side=tk.TOP, fill=tk.X)
 
-        ################# Results
+        # ################ Results
 
         self.results_frame = tk.Frame(self.window)
         self.results_frame.config(borderwidth=4, relief=tk.GROOVE)
         self.results_options_frame = tk.Frame(self.results_frame)
-        self.open_file_button = tk.Button(self.results_options_frame, text="Open File")
-        self.open_folder_button = tk.Button(self.results_options_frame, text="Open Folder")
+        self.open_file_button = tk.Button(self.results_options_frame, text="Open File", command=self.open_file)
+        self.open_folder_button = tk.Button(self.results_options_frame, text="Open Folder", command=self.open_folder)
         self.open_file_button.pack(side=tk.LEFT)
         self.open_folder_button.pack(side=tk.LEFT)
         self.results_options_frame.pack()
 
-        ################# Results Scroll Box
+        # ################ Results Scroll Box
 
         self.results_frame_scroll = tk.Frame(self.results_frame)  # select of names
         self.scrollL = tk.Scrollbar(self.results_frame_scroll, orient=tk.VERTICAL)
@@ -82,7 +89,7 @@ class Window(Thread):
         self.results_frame_scroll.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
         self.results_listbox.config(width=300)
         self.results_listbox.pack(fill=tk.Y, expand=1)
-        self.results_listbox.bind('<<ListboxSelect>>', self.a)
+        # self.results_listbox.bind('<<ListboxSelect>>', self.a) # No single click action needed
         self.results_listbox.bind('<Double-Button-1>', self.a)
 
         self.results_frame.pack(fill=tk.BOTH, expand=1)
@@ -90,20 +97,43 @@ class Window(Thread):
     def a(self, asdf):
         pass
 
-    def launchPutty(self):
-        self.putty.start()
+    def start_scan(self):
+        path = self.scan_text.get()
+        print "Would now scan ", path
+        # TODO make this start a scan running.
 
-    def stopParseRMA(self):
-        self.RMAThread.go = 0
-        self.startRMA.config(text="Quit Program", command=quit)
+    def browse_scan_path(self):
+        fullpath = tkFileDialog.askdirectory(initialdir="C:")
+        self.scan_text.insert(0, fullpath)
 
-    def stopGetRMA(self):
-        self.RMAThread.substate = 100
-        self.startRMA.config(text="Launch Putty", command=self.openPutty)
+    def search(self, event=""):
+        search_text = self.search_text.get()
+        results = self.scanner.get_folders("%" + search_text + "%")
+        self.results_listbox.delete(0, tk.END)
+        for result in results:
+            self.results_listbox.insert(0, os.path.join(result[0], result[1]))
 
-    def openPutty(self):
-        self.launchPutty()
-        self.startRMA.config(text="Stop importing RMA numbers", command=self.stopParseRMA)
+    def open_file(self):
+        try:
+            index = int(self.results_listbox.curselection()[0])
+            path = self.results_listbox.get(index)
+            print "Opening ", path
+        except IndexError:
+            pass
+
+    def open_folder(self):
+        path = ""
+        try:
+            index = int(self.results_listbox.curselection()[0])
+            path = self.results_listbox.get(index)
+            print "Opening ", path
+        except IndexError:
+            pass
+        if path:
+            folder_path, filename = os.path.split(path)
+            command = "explorer /Select \"{0}\"".format(path)
+            print command
+            subprocess.call(command, shell=True)
 
 
     def startGetRMA(self):
