1866251e1c603469e6a42461638ea2d2208db055
Ben Holleran <Wild_Doogy@yahoo.com>
2015-04-17 10:46:51 -0400

    Need to add an actual Reading to add the scanning queue

--- Scanner.py
+++ Scanner.py
@@ -1,7 +1,14 @@
 __author__ = 'boh01'
 
 from threading import Thread
-import os
+
+# Needed for scanning
+from multiprocessing.dummy import Pool as ThreadPool
+from threading import Lock
+import Queue
+
+import os, time
+import datetime, shutil # Needed for backup
 
 import DirectoryDB
 
@@ -11,13 +18,25 @@ class Scanner(Thread):
         Thread.__init__(self)
         self.directory_dictionary = {}
         self.init_db()
+        self.start_time = time.time()
+
+        self.number_of_threads = 16
+        self.update_pool = ThreadPool(self.number_of_threads)
+        self.update_pool.thread_count = 0
+        self.update_pool.thread_lock = Lock()
+        self.update_pool.messages = Queue.Queue()
+
+
 
     def init_db(self):
         FindIt = self.create_FindIt()
         if not FindIt:
             print "Could not create the FindIt directory. Will now crash."
             quit()
-        self.directory_database = DirectoryDB.DirectoryDB(os.path.join(FindIt, "FindIt.db"))
+        database_filename = os.path.join(FindIt, "FindIt.db")
+        self.backup_db(database_filename)
+        self.directory_database = DirectoryDB.DirectoryDB(database_filename)
+        self.directory_database.start()
 
 
     def create_FindIt(self):
@@ -33,7 +52,8 @@ class Scanner(Thread):
             return False
 
     def load_preferences(self):
-
+        # import csv
+        # TODO need to create a config file
         pass
 
 
@@ -43,8 +63,23 @@ class Scanner(Thread):
     def run(self):
         pass
 
-    def scan_dir(self, dir):
-        pass
+    def scan_dir(self, folder_to_scan):
+        if not os.path.isdir(folder_to_scan):  # Make sure the folder exists
+            print "Cannot access the folder to be scanned:", folder_to_scan
+            raw_input("Press enter to exit")
+            exit()
+        else:
+            pass
+
+    def backup_db(self, pathToDB):
+        try:
+            nm = pathToDB + str(datetime.datetime.now()).replace(":", "-") + ".backup"
+            print nm
+            if os.path.isfile(pathToDB):
+                shutil.copy(pathToDB, nm)  # Move old database to a backup location
+                print "Output file backed up."
+        except ValueError:
+            print "Output file not backed up. File may not exist, permissions, etc. This might be a problem later"
 
 
 a = Scanner()
--- main.py
+++ main.py
@@ -4,21 +4,9 @@ import shutil
 from multiprocessing.dummy import Pool as ThreadPool
 from threading import Lock
 
-from Directory import *
 import Scanner
-import DirectoryDB
 import GUI
 
-
-def backup_db(pathToDB):
-    try:
-        nm = pathToDB + str(datetime.datetime.now()).replace(":", "-") + ".backup"
-        print nm
-        if os.path.isfile(pathToDB):
-            shutil.copy(pathToDB, nm)  # Move old database to a backup location
-            print "Output file backed up."
-    except ValueError:
-        print "Output file not backed up. File may not exist, permissions, etc. This might be a problem later"
 
 
 if __name__ == '__main__':
@@ -37,26 +25,12 @@ if __name__ == '__main__':
     last_update_date = 0.0 # datetime.datetime(1990, 1, 1)
     pathToOutputCSV = os.path.join(db_folder, db_file)
 
-    number_of_threads = 16
-    update_pool = ThreadPool(number_of_threads)
-    update_pool.thread_count = 0
-    update_pool.thread_lock = Lock()
-    update_pool.messages = Queue.Queue()
 
-    backup_db(DB_path)
-    DB = DirectoryDB.DirectoryDB(DB_path)
-    DB.start()
 
-    # TODO need to create a config file
 
-    if not os.path.isdir(FolderToScan):  # Make sure the folder exists
-        print "Cannot access the folder to be scanned:", FolderToScan
-        raw_input("Press enter to exit")
-        exit()
 
-    startTime = time.time()  # Write down time for later
 
-    DirectoryDictionary = {}
+
     """ :type DirectoryDictionary: dict of Directory"""
     DirectoryDictionary[FolderToScan] = Directory(FolderToScan, last_update_date, DirectoryDictionary)
     # Above plants a seed at the base of the folder tree. Any folders created before the date will not be scanned
