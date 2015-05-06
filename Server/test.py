9994dde37c7127f36da37a3c5b1bf486b3101957
Ben Holleran <Wild_Doogy@yahoo.com>
2015-05-06 11:20:31 -0400

    Messing about with memory profileing

--- Server/test.py
+++ Server/test.py
@@ -1,14 +1,38 @@
-__author__ = 'boh01'
+from memory_profiler import profile
 
-import socket
+@profile(precision = 16)
+def my_func():
+    import random
+    import string
+    import sys
+    import time
 
-go = True
-while go:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("10.196.112.99", 9091))
-    msg = raw_input()
-    if msg == "q":
-        g = 0
-    s.send(msg)
-    print s.recv(10000000)
-    s.close()
+    for j in xrange(1000*100):
+        pass
+
+    d = {}
+    for i in xrange(1000*10):
+        s = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        v = random.random()
+        d[s] = v
+        if i%1000 == 0:
+            print i
+    print sys.getsizeof(d)
+    time.sleep(3)
+    d = {}
+    time.sleep(3)
+    for i in xrange(1000*10):
+        s = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        v = random.random()
+        d[s] = v
+        if i%1000 == 0:
+            print i
+    time.sleep(3)
+    del d
+    d = {}
+    time.sleep(3)
+
+if __name__ == '__main__':
+    my_func()
+
+
