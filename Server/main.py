d0854689870e114f14dee5e3c9151c2bafa59793
Ben Holleran <Wild_Doogy@yahoo.com>
2015-05-13 16:04:38 -0400

    Scanning started

--- Server/main.py
+++ Server/main.py
@@ -110,7 +110,7 @@ class FindIt(Thread):
 if __name__ == '__main__':
     version = "2.0.0"
     F = FindIt(version)
-    F.scanner.scan_dir("/media/M/Drawings")
+    F.scanner.scan_dir("/media/M/")
     F.start()
 
 
