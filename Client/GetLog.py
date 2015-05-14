93d9fa9c8cbc4f85ac06318bf81cc134253320ac
Ben Holleran <Wild_Doogy@yahoo.com>
2015-05-14 13:16:36 -0400

    Added a thing to retrieve the log data

---
+++ Client/GetLog.py
@@ -0,0 +1,91 @@
+__author__ = 'boh01'
+
+
+import time
+import re
+from threading import Thread
+import socket, os
+
+def getlocalip():
+    import os
+    if os.sys.platform == "win32":
+        back = os.popen("ipconfig /all")
+        cmd = back.read(2000)
+        cmd2 = cmd[cmd.find("IP Address"):cmd.find("IP Address")+70]
+        cmd3 = cmd2[cmd2.find(":")+2:cmd2.find(":")+19]
+        c4 = cmd3[0:cmd3.find(" ")-2]
+
+
+
+    if os.sys.platform == "linux2":
+        back = os.popen("ifconfig")
+        cmd = back.read(2000)
+        cmd2 = cmd[cmd.find("Ethernet"):cmd.find("Ethernet")+300]
+        cmd3 = cmd2[cmd2.find("inet addr:")+10:cmd2.find("inet addr:")+50]
+        c4 = cmd3[0:cmd3.find(" ")]
+
+    if c4 != "":
+        return c4
+    else:
+        return "128.238.66.216"
+
+
+
+def getadd():
+    local = getlocalip()
+    host = raw_input("Type host, skip for "+str(local)+ ":")
+    if host == "":
+        host = local
+    port = raw_input("Type port, skip for 45678 :")
+    if port:
+        port = int(port)
+    else:
+        port = 45678
+    address = (host,port)
+    return address
+
+def setup(address):
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client.connect(address)
+    return client
+
+class ThreadClass(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+    def run(self):
+        pass
+
+def startclass():
+    TClass= ThreadClass()
+    TClass.start()
+    return TClass
+
+
+
+
+def mainprog(client):
+    i = 0
+    while 1:
+        i+=1
+        try:
+            client.send("log")
+            data = client.recv(20000000)
+            while data:
+                print data
+                data = client.recv(20000000)
+        except:
+            print "Socket closed. Reconnecting."
+            break
+        if data == "":
+            break
+        print data," ",i
+        if len(data) > 40:
+            print "Sending"
+            client.send(data)
+
+while 1:
+    client = setup(getadd())
+    mainprog(client)
+
+
+
