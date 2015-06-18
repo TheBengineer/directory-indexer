a5898f0347f85cabc7ad4a9d7af2cb9130c3f008
Ben Holleran <Wild_Doogy@yahoo.com>
2015-06-18 12:12:57 -0400

    Adding in a scanner to the client.

--- Client/main.py
+++ Client/main.py
@@ -1,10 +1,13 @@
 __author__ = 'Wild_Doogy'
 
 import GUI
+import Scanner
 
 
 if __name__ == '__main__':
     version = "2.0.1"
+    localScan = Scanner.Scanner()
+    localScan.start()
     gui = GUI.Window()
     gui.version = version
     gui.start()
