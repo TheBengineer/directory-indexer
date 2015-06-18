6957e58049db3e0fe8f4dae33c0ffc3c63553578
Ben Holleran <Wild_Doogy@yahoo.com>
2015-06-18 11:56:00 -0400

    Moved a bunch of folders over from the server side of things, So I can add local scanning capability.

---
+++ Client/DirectoryDB.py
@@ -0,0 +1,241 @@
+__author__ = 'Ben'
+import sqlite3 as lite
+
+from threading import Thread
+from threading import Lock
+import time
+import os, sys
+
+"""
+This database will hold all the files and paths that are normally written to a CSV file.
+
+"""
+
+
+def log(*args):
+    print "[DirectoryDB]",
+    print time.strftime("%c"),
+    print " ",
+    for arg in args:
+        print arg,
+    print ""
+
+
+class DirectoryDB(Thread):
+    def __init__(self, file_path, GUI=None):
+        # TODO make this save data more efficiently
+        Thread.__init__(self)
+        self.lock = Lock()
+        self.local_lock = Lock()
+        self.lock.acquire()
+        self.file_path = file_path
+        self.DB = lite.connect(self.file_path, check_same_thread=False)
+        self.DB.text_factory = str
+        self.DB_cursor = self.DB.cursor()
+        self.lock.release()
+        self.create_tables()
+        self.files_to_add = []
+        self.files_to_delete = []
+        self.folders_to_delete = []
+        self.GUI = GUI
+        self.go = 1
+        self.platform = sys.platform
+        """
+        :type self.lock: threading.Lock
+        :type self.local_lock: threading.Lock
+        """
+
+    def create_tables(self):
+        create_table_files = "CREATE TABLE files " \
+                             "(directory INTEGER, filename TEXT, scan_time REAL, CONSTRAINT unq UNIQUE (directory, filename));"
+        create_table_directories = "CREATE TABLE directories " \
+                                   "(id INTEGER PRIMARY KEY, path TEXT, scan_time REAL, CONSTRAINT unq UNIQUE (path));"
+        self.lock.acquire()
+        tables = self.DB_cursor.execute("SELECT name FROM sqlite_master"
+                                        " WHERE type='table'").fetchall()
+        log("Found tables:", tables, "in the DB.")
+        if ("files",) not in tables:
+            self.DB_cursor.execute(create_table_files)
+        if ("directories",) not in tables:
+            self.DB_cursor.execute(create_table_directories)
+        self.lock.release()
+
+    def add_file(self, file_path):
+        path, filename = os.path.split(file_path)
+        self.add_fileB(path, filename)
+
+    def add_fileB(self, path, filename):
+        if filename and path:
+            self.local_lock.acquire()
+            self.files_to_add.append([path, filename])
+            self.local_lock.release()
+            if self.GUI:
+                self.GUI.add_scanned_path(os.path.join(path, filename))
+
+    def del_folder(self, folder_path):
+        if folder_path:
+            self.local_lock.acquire()
+            self.folders_to_delete.append(folder_path)
+            self.local_lock.release()
+
+    def del_file(self, file_path):
+        path, filename = os.path.split(file_path)
+        if filename and path:
+            self.files_to_delete.append([path, filename])
+
+    def get_path_id(self, path, scan_time=0.0):
+        query = "INSERT OR IGNORE INTO directories(path, scan_time) VALUES(\"{0}\", {1});".format(path, scan_time)
+        query2 = "SELECT directories.id FROM directories WHERE path LIKE \"{0}\";".format(path)
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        self.DB_cursor.execute(query2)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        if len(data):
+            return data[0][0]
+        else:
+            return -1
+
+    def get_path_time(self, path):
+        query = "SELECT directories.scan_time FROM directories WHERE path LIKE \"{0}\";".format(path)
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        if len(data):
+            return data[0][0]
+        else:
+            return 0.0
+
+    def fix_path(self, path):
+        if self.platform == "win32":
+            if path.startswith("/media/"):
+                path2 = path[7:]  # Slice off the leading "/media/"
+                drive = path2[0]  # Grab the next char
+                path = drive + ":" + path[8:]  # TODO this is hardcoded to my drive system
+            return path.replace("/", "\\")
+        elif self.platform == "linux2":
+            if path[1]== ":":
+                drive = path[0].upper()  # Grab the next char
+                path = "/media/" + drive + "/" + path[2:]  # TODO this is hardcoded to my drive system
+            return path.replace("\\", "/")
+        else:
+            return path # TODO Make a Mac version?
+
+    def run(self):
+        """
+        :return:
+        """
+
+        while self.go:
+            self.local_lock.acquire()
+            if len(self.files_to_add):
+                for path, filename in self.files_to_add:
+                    path = self.fix_path(path)
+                    path_id = self.get_path_id(path, time.time())  # TODO this takes a long time. Fix this.
+                    query = "INSERT OR REPLACE INTO files (directory, filename, scan_time) VALUES(\"{path_id}\", " \
+                            " \"{filename}\", \"{time}\");".format(path_id=path_id, filename=filename,
+                                                                   time=time.time())
+                    self.lock.acquire()
+                    try:
+                        self.DB_cursor.execute(query)
+                    except lite.OperationalError:
+                        log("ERROR, could not add file: ", query)
+                    self.lock.release()
+            self.files_to_add = []
+            if len(self.files_to_delete):
+                for path, filename in self.files_to_delete:
+                    path = self.fix_path(path)
+                    query = "DELETE FROM files WHERE path ='{path}' AND filename ='{filename}'" \
+                            " ;".format(path=path, filename=filename)
+                    self.lock.acquire()
+                    self.DB_cursor.execute(query)
+                    self.lock.release()
+            self.files_to_delete = []
+            if len(self.folders_to_delete):
+                for path in self.folders_to_delete:
+                    path = self.fix_path(path)
+                    path_id = self.get_path_id(path)
+                    query = "DELETE FROM files WHERE directory = {0}".format(path_id)
+                    query2 = "DELETE FROM directories WHERE id = {0}".format(path_id)
+                    log("Deleting path", path, query, query2)
+                    self.lock.acquire()
+                    self.DB_cursor.execute(query)
+                    self.DB_cursor.execute(query2)
+                    self.lock.release()
+            self.folders_to_delete = []
+            self.local_lock.release()
+            self.writeout()
+            # TODO is 30 seconds a good time for the database to be written to?
+            time.sleep(.1)
+
+    def get_folders(self, filename):
+        query = "SELECT directories.path, f.filename FROM files f JOIN directories ON f.directory=directories.id WHERE f.filename LIKE '{filename}';".format(
+            filename=filename)
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        return data
+
+    def get_folders_500(self, filename):
+        query = "SELECT directories.path, f.filename FROM files f JOIN directories ON f.directory=directories.id WHERE f.filename LIKE '{filename}' LIMIT 500;".format(
+            filename=filename)
+        log("Getting results for ", query)
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        return data
+
+    def get_folders_limit(self, filename, limit=500):
+        query = "SELECT directories.path, f.filename  FROM files f JOIN directories ON f.directory=directories.id WHERE f.filename LIKE '{filename}' LIMIT {limit};".format(
+            filename=filename, limit=limit)
+        log("Getting results for ", query)
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        return data
+
+    def writeout(self):
+        self.lock.acquire()
+        self.DB.commit()
+        self.lock.release()
+
+    def dump(self):
+        query = "SELECT path, filename, scan_time FROM files;"
+        # TODO need a join here
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        return data
+
+    def dump_paths(self):
+        query = "SELECT DISTINCT path, scan_time FROM directories;"
+        log("Starting to dump all stored paths. This may take a while.")
+        self.lock.acquire()
+        self.DB_cursor.execute(query)
+        data = self.DB_cursor.fetchall()
+        self.lock.release()
+        log("Done dumping all paths.")
+        return data
+
+    def dump_paths_dict(self, dict):
+        data = self.dump_paths()
+        result = []
+        for path, time_ in data:
+            path_fixed = self.fix_path(path)
+            dict[path_fixed] = time_
+            result.append((path_fixed, time_))
+        return result
+
+    def nuke(self):
+        query1 = "DELETE FROM files;"
+        query2 = "DELETE FROM directories;"
+        self.lock.acquire()
+        self.DB_cursor.execute(query1)
+        self.DB_cursor.execute(query2)
+        self.DB_cursor.execute("VACUUM;")
+        self.lock.release()
---
+++ Client/Scanner.py
@@ -0,0 +1,279 @@
+__author__ = 'boh01'
+
+from threading import Thread
+
+# Needed for scanning
+from multiprocessing.pool import ThreadPool as Pool_for_map
+from multiprocessing.dummy import Pool as ThreadPool
+
+from threading import Lock
+import Queue
+
+# Needed for backup
+import datetime, shutil
+
+import gc
+import os, time, sys
+import string
+
+import DirectoryDB
+import Directory
+
+import scandir as myScandir
+
+
+def log(*args):
+    print "[Scanner]",
+    print time.strftime("%c"),
+    print " ",
+    for arg in args:
+        print arg,
+    print ""
+
+
+class Scanner(Thread):
+    def __init__(self, GUI=None):
+        Thread.__init__(self)
+        """
+        :type GUI: GUI.Window
+        :return:
+        """
+        self.linux = (sys.platform == "linux2")
+
+        self.GUI = GUI
+
+        self.directory_dictionary = {}
+        self.directory_database = self.init_database()
+        self.roots = []
+        """ :type self.directory_dictionary: dict of Directory.Directory"""
+        self.start_time = time.time()
+
+        self.number_of_threads = 16
+        self.update_pool = ThreadPool(self.number_of_threads)
+        self.update_pool.thread_count = 0
+        self.update_pool.thread_lock = Lock()
+        self.update_pool.messages = Queue.Queue()
+
+        self.directories_to_refresh = []
+        self.directories_to_scan = []
+        self.scan_results = [[], [], []]
+        self.time_cache = {}
+
+        self.scan_pool = Pool_for_map(128)
+
+        self.go = 1
+        self.log = ""
+        self.last_update = time.time()
+        self.update_interval = 60 * 60 * 12  # Seconds
+        # TODO make this scan at a time, say 8 PM
+
+    def init_database(self):
+        FindIt = self.create_FindIt_folder()
+        if not FindIt:
+            log("Could not create the FindIt directory. Will now crash.")
+            quit()
+        database_filename = os.path.join(FindIt, "FindItV2.db")
+        # self.backup_db(database_filename)
+        directory_database = DirectoryDB.DirectoryDB(database_filename, self.GUI)
+        directory_database.start()
+        return directory_database
+
+    def create_FindIt_folder(self):
+        if sys.platform == "win32":
+            appdata = os.getenv('APPDATA')
+            FindIt = os.path.join(appdata, "FindIt")
+            if not os.path.isdir(FindIt):
+                os.mkdir(FindIt)
+            if os.path.isdir(FindIt):
+                return FindIt
+            else:
+                return False
+        elif self.linux:
+            home = os.getenv('HOME')
+            FindIt = os.path.join(home, "FindIt")
+            if not os.path.isdir(FindIt):
+                os.mkdir(FindIt)
+            if os.path.isdir(FindIt):
+                return FindIt
+            else:
+                return False
+        else:
+            log("Crashing. Not made to run on mac")
+
+    def load_preferences(self):
+        # import csv
+        # TODO need to create a config file
+        pass
+
+    def refresh_folder(self, (path, scan_time)):
+        try:
+            mtime = os.path.getmtime(path)
+            if mtime > scan_time:
+                return (0, mtime)  # The folder was updated.
+            else:
+                return (1, mtime)  # The folder was NOT updated.
+        except os.error:  # Not accessible.
+            return (2, 0.0)  # The folder non-accessible.
+
+    def scan_folder(self, path):
+        (pathS, directoriesS, filesS) = ((), (), ())
+        try:
+            for (pathS, directoriesS, filesS) in myScandir.walk(path):
+                break
+            return (pathS, directoriesS, filesS)
+        except os.error:
+            log("Path", path, "is not accessible.")
+            # except TypeError:
+            #    log("Path Seems to not exist:", path)
+
+    def linux_path(self, path):
+        if path[1] == ":":
+            drive = path[0].upper()
+            new_path = "/media/" + drive + "/" + string.replace(path[3:], "\\", "/")
+            return new_path
+        else:
+            return False
+
+    def schedule_refresh(self, path, last_scan_time):
+        if path and last_scan_time:
+            self.directories_to_refresh.append((path, last_scan_time))
+        else:
+            log("Trying to schedule a refresh on invalid path/time:", last_scan_time, path)
+
+    def run(self):
+        # gc.disable()
+        gc.set_threshold(10)
+
+        time.sleep(.1)
+        log("Roots:", self.roots)
+        self.directories_to_refresh += self.directory_database.dump_paths_dict(self.time_cache)
+        t2 = time.time()
+        while self.go:
+            if not len(self.directories_to_refresh) and not len(self.directories_to_scan):
+                time.sleep(1)
+            else:
+                tmp_to_freshen = []
+                for i in xrange(min(len(self.directories_to_refresh), 512)):  # Get the next 512 directories to freshen
+                    try:
+                        (path, scan_time) = self.directories_to_refresh.pop()
+                        if self.linux:
+                            l_path = self.linux_path(path)
+                            if l_path:
+                                tmp_to_freshen.append((l_path, scan_time))
+                            elif path[:7] == '/media/':
+                                tmp_to_freshen.append((path, scan_time))  # Already in linux format.
+                                # log("Path seems to be already linux", path)
+                            else:
+                                log("Path could not be converted to linux.", path)
+                        else:
+                            tmp_to_freshen.append((path, scan_time))
+                    except IndexError:
+                        break
+                # log("To freshen", tmp_to_freshen)
+                t3 = time.time()
+                delta = t3 - t2
+                log("Refresh prep overhead: ", delta, "Seconds (", round(delta * 300), "~Folders)")
+                t = time.time()
+                results_fresh = self.scan_pool.map(
+                    lambda (path_scan_time): self.refresh_folder(path_scan_time), tmp_to_freshen)
+                t2 = time.time()
+                delta = max(t2 - t, .001)
+                log("Refreshed ", len(tmp_to_freshen), "in ", delta, "Seconds (", len(tmp_to_freshen) / delta,
+                    "Folders/Second)")
+                # log("Fresh results", results_fresh)
+                for index, (result, mtime) in enumerate(results_fresh):
+                    path = tmp_to_freshen[index][0]
+                    # log("Processing ", result, index, tmp_to_freshen[index][0])
+                    if result == 0:
+                        # log("Adding", tmp_to_freshen[index][0])
+                        self.directories_to_scan.append(path)
+                        self.time_cache[path] = mtime
+                        #log("Adding path to cache:", path)
+                    elif result == 1:
+                        self.time_cache[path] = mtime
+                        #log("Adding path to cache:", path)
+                    elif result == 2:
+                        # directory needs to be deleted from DB.
+                        self.directory_database.del_folder(tmp_to_freshen[index][0])
+                # log("To scan:", self.directories_to_scan)
+                tmp_to_scan = []
+                for i in xrange(min(len(self.directories_to_scan), 512)):  # Get the next 512 directories to freshen
+                    try:
+                        tmp_to_scan.append(self.directories_to_scan.pop())
+                    except IndexError:
+                        break
+                # log("To Scan", tmp_to_scan)
+                t3 = time.time()
+                delta = t3 - t2
+                log("Post refresh overhead: ", delta, "Seconds (", round(delta * 300), "~Folders)")
+                t = time.time()
+                results_scan = self.scan_pool.map(self.scan_folder, tmp_to_scan)
+                t2 = time.time()
+                delta = max(t2 - t, .001)
+                log("Scanned ", len(tmp_to_scan), "in ", delta, "Seconds (", len(tmp_to_scan) / delta,
+                    "Folders/Second)")
+                # log("Scan results", results_scan)
+                for (path, directories, files) in results_scan:
+                    for directory in directories:
+                        if self.linux:
+                            if not path[-1] == "/":
+                                folderpath = path + "/" + directory
+                            else:
+                                folderpath = path + directory
+                        else:
+                            if not path[-1] == "\\":
+                                folderpath = path + "\\" + directory
+                            else:
+                                folderpath = path + directory
+                        # log("Debug", folderpath, self.time_cache) # This breaks all the things.
+                        # log("Debug", folderpath)
+                        if folderpath in self.time_cache:
+                            scan_time = self.time_cache[folderpath]
+                        else:
+                            scan_time = 0.0
+                            #log("Path not in cache", folderpath)
+                        self.directories_to_refresh.append(
+                            (os.path.join(path, directory), scan_time))
+                    for file in files:
+                        self.directory_database.add_fileB(path, file)
+                self.directory_database.writeout()
+                t3 = time.time()
+                delta = t3 - t2
+                log("Post scan overhead: ", delta, "Seconds (", round(delta * 300), "~Folders)")
+                t2 = time.time()
+            if time.time() - self.last_update > self.update_interval:
+                self.directories_to_refresh += self.directory_database.dump_paths()
+                for root_dir in self.roots:
+                    self.schedule_refresh(root_dir, 0.0)
+
+    def add_to_roots(self, folder_to_scan):
+        if not os.path.isdir(folder_to_scan):  # Make sure the folder exists
+            log("Cannot access the folder to be scanned: ", folder_to_scan)
+        else:
+            self.directories_to_refresh.append((folder_to_scan, 0.0))
+            self.roots.append(folder_to_scan)
+
+    def scan_dir(self, folder_to_scan):  # TODO Delete this
+        self.add_to_roots(folder_to_scan)
+        if os.path.isdir(folder_to_scan):  # Make sure the folder exists
+            # log("Attempting to scan path ", folder_to_scan) # Not needed
+            if folder_to_scan not in self.directory_dictionary:
+                self.directory_dictionary[folder_to_scan] = Directory.Directory(folder_to_scan, 0.0,
+                                                                                self.directory_dictionary,
+                                                                                self)  # Create Root and reset time.
+            self.update_pool.apply_async(self.directory_dictionary[folder_to_scan].update,
+                                         args=(self.update_pool, self.directory_database,))  # Go. Scan. Be Free.
+            if self.GUI:
+                self.GUI.set_status("Scanning: " + folder_to_scan)
+            else:
+                log("Scanning: ", folder_to_scan)
+
+    def backup_db(self, pathToDB):
+        try:
+            nm = pathToDB + str(datetime.datetime.now()).replace(":", "-") + ".backup"
+            log(nm)
+            if os.path.isfile(pathToDB):
+                shutil.copy(pathToDB, nm)  # Move old database to a backup location
+                log("Output file backed up.")
+        except ValueError:
+            log("Output file not backed up. File may not exist, permissions, etc. This might be a problem later")
