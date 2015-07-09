d1f52b2f1e36744c8a58ac99e0195e5cb07081b0
Ben Holleran <Wild_Doogy@yahoo.com>
2015-07-09 17:35:13 -0400

    Good things?

--- Server/main.py
+++ Server/main.py
@@ -29,7 +29,7 @@ class FindIt(Thread):
         self.go = True
         self.clients = {}
         self.scanner = Scanner.Scanner()
-        # self.scanner.start()
+        self.scanner.start()
         self.search_server = SearchServer.SearchServer(self.scanner)
         self.search_server.start()
         self.log_server = LogServer.LogServer(self.scanner)
@@ -72,54 +72,4 @@ if __name__ == '__main__':
     F.start()
     s = F.scanner
     d = s.directory_database
-    data = d.dump_paths_ids()
-    log("Done dumping", len(data), "dirs in DB")
-    for dir, id in data:
-        d.folders[dir] = id
-    log("Dict built")
-    del data
-    import os
 
-    folders_to_add = {}
-    if os.path.isfile("/tmp/data"):
-        import csv
-
-        f = open("/tmp/data", 'rt')
-        try:
-            reader = csv.reader(f)
-            index = 0
-            for row in reader:
-                if index % 100000 == 0:
-                    log(index, "files scanned", len(folders_to_add), "unique folders.")
-                if not len(row) == 2:
-                    log("bad row", row)
-                else:
-                    if row[0] not in d.folders:
-                        if row[0] not in folders_to_add:
-                            folders_to_add[row[0]] = d.fix_path(row[0], "DB")
-                            if folders_to_add[row[0]] not in d.folders:
-                                log("Still need to add", row[0])
-                        dir_id = d.folders[folders_to_add[row[0]]]
-                        query = "INSERT OR REPLACE INTO files (directory, filename, scan_time) VALUES(\"{0}\", " \
-                            " \"{1}\", \"{2}\");".format(folders_to_add[row[0]], row[1], time.time())
-                        d.DB_cursor.execute(query)
-                    else:
-                        log("Row exists?", row)
-
-                index += 1
-        finally:
-            f.close()
-
-            # log(len(folders_to_add), "folders still to be added")
-            # log("Found", len(folders_to_add), "Folders that need to be added. (wrong path format, so useless)")
-            # corrected_path_names = []
-            # for pathname in folders_to_add:
-            #    corrected_path_names.append(d.fix_path(pathname, "DB"))
-            # log(len(corrected_path_names),"corrected path names, like:", corrected_path_names[0])
-
-            # t = time.time()
-            # d.funnel_folders(corrected_path_names[:100])
-            # d.funnel_folders(corrected_path_names)
-            # data = d.dump_paths_ids()
-
-            # log("can add", time.time()-t *100, "Folders / Second")
