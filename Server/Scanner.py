349d4bbd76612b1d19367eed5a6c5df8bf54875f
Ben Holleran <Wild_Doogy@yahoo.com>
2015-07-20 14:52:35 -0400

    Catches empty paths

--- Server/Scanner.py
+++ Server/Scanner.py
@@ -161,22 +161,25 @@ class Scanner(Thread):
                         (self.path, scan_time) = self.directories_to_refresh.pop()
                         if self.linux:
                             self.l_path = self.linux_path(self.path)
-                            if self.l_path:
-                                if "//" in self.l_path:
-                                    self.directory_database.del_folder(self.l_path)
-                                    self.l_path = self.l_path.replace("//","/")
-                                    while "//" in self.l_path:
+                            try:
+                                if self.l_path:
+                                    if "//" in self.l_path:
+                                        self.directory_database.del_folder(self.l_path)
                                         self.l_path = self.l_path.replace("//","/")
-                                self.tmp_to_freshen.append((self.l_path, scan_time))
-                            elif self.path[:7] == '/media/':
-                                if "//" in self.path:
-                                    self.path = self.path.replace("//","/")
-                                    while "//" in self.path:
+                                        while "//" in self.l_path:
+                                            self.l_path = self.l_path.replace("//","/")
+                                    self.tmp_to_freshen.append((self.l_path, scan_time))
+                                elif self.path[:7] == '/media/':
+                                    if "//" in self.path:
                                         self.path = self.path.replace("//","/")
-                                self.tmp_to_freshen.append((self.path, scan_time))  # Already in linux format.
-                                # log("Path seems to be already linux", path)
-                            else:
-                                log("Path could not be converted to linux.", self.path)
+                                        while "//" in self.path:
+                                            self.path = self.path.replace("//","/")
+                                    self.tmp_to_freshen.append((self.path, scan_time))  # Already in linux format.
+                                    # log("Path seems to be already linux", path)
+                                else:
+                                    log("Path could not be converted to linux.", self.path)
+                            except TypeError:
+                                log("Malformed path", self.path, scan_time)
                         else:
                             self.tmp_to_freshen.append((self.path, scan_time))
                     except IndexError:
